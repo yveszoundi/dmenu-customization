9980b798762c63f6c00db9441ae97ceb749af4e7
Yves Zoundi <yveszoundi@gmail.com>
2022-04-05 09:32:23 -0400

    Initial commit

---
+++ config.mk
@@ -0,0 +1,18 @@
+# dmenu version
+VERSION = 5.1
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+# Linking
+FREETYPELIBS = -lfontconfig -lXft
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# compiler and linker
+CC = cc
+
+UNAME != uname
+
+include config.mk.${UNAME}
