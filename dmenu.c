e717a8225fbcd6de281a41ec756c56c3034dacbf
Yves Zoundi <yveszoundi@gmail.com>
2022-04-19 06:49:27 -0400

    Address crash
    Apply dmenu-flexipatch patch from https://github.com/bakkeby/dmenu-flexipatch/commit/e8da68a1a0d99b94706e53118604751599a90a81

--- dmenu.c
+++ dmenu.c
@@ -639,10 +639,7 @@ setup(void)
 	for (j = 0; j < SchemeLast; j++) {
 		scheme[j] = drw_scm_create(drw, (const char**)colors[j], 2);
 	}
-	for (j = 0; j < SchemeOut; ++j) {
-		for (i = 0; i < 2; ++i)
-			free(colors[j][i]);
-	}
+	
 
 	clip = XInternAtom(dpy, "CLIPBOARD",   False);
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
@@ -862,7 +859,7 @@ main(int argc, char *argv[])
 	if (!drw_fontset_create(drw, (const char**)fonts, LENGTH(fonts)))  
 		die("no fonts could be loaded.");
 
-	free(fonts[0]);  
+	
 	lrpad = drw->fonts->h;
 
 #ifdef __OpenBSD__
